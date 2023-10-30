751041671c999f3d10a49a9a59b4086919ac991f
Timur <tima.temirov.2005@mail.ru>
2023-10-30 17:49:07 +0300

    Add verbose name to apps

--- Django/robotechnics/achievement/apps.py
+++ Django/robotechnics/achievement/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AchievementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'achievement'
+    verbose_name = 'Достижения'
