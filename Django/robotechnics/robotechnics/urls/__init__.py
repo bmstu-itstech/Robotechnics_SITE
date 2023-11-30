8469f1879bb530a18a788d838cbbe430d79a094b
Timur <tima.temirov.2005@mail.ru>
2023-11-30 15:49:31 +0300

    swagger-2.

--- Django/robotechnics/robotechnics/urls/__init__.py
+++ Django/robotechnics/robotechnics/urls/__init__.py
@@ -32,10 +32,10 @@ if settings.DEBUG:
         path('api/swagger/', TemplateView.as_view(
             template_name='swagger/swagger.html',
             extra_context={'schema_url': 'openapi-schema'}
-        ), name='swagger'),
-        re_path(
-            r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0),
+            ), name='swagger'
+        ),
+        path(
+            'api/swagger<format>/', schema_view.without_ui(cache_timeout=0),
             name='schema-json'
         ),
     ]
