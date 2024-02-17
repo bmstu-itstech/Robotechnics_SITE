c7334543f702cdafe360c4d042d63aa53b4db3c2
nikvoen <nikvoen@yandex.ru>
2024-02-17 18:09:04 +0300

    Create carousel for News Page with mobile view v1.0

--- Frontend/src/components/pages/NewsPage/NewsPage.tsx
+++ Frontend/src/components/pages/NewsPage/NewsPage.tsx
@@ -1,16 +1,51 @@
+import React from 'react';
 import "./newspage.scss"
 import Circle from "../../utils/circle/Circle"
 import NewsCard from "../../utils/news-card/NewsCard";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const responsive = {
+    0: { items: 1 },
+    1100: { items: 2 },
+    1650: { items: 3 },
+    2350: { items: 4 }
+};
+
+const items = [
+    <NewsCard img="news1.png"/>,
+    <NewsCard img="news2.png"/>,
+    <NewsCard img="news3.png"/>,
+    <NewsCard img="news3.png"/>
+];
+
+const Carousel = () => (
+    <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
+        disableButtonsControls={true}
+        infinite={true}
+    />
+);
 
 export const NewsPage = () => {
     return (
         <section className={"news-page"}>
             <Circle title="новости"/>
             <div className="news-carousel">
-                <NewsCard img="news1.png"/>
-                <NewsCard img="news2.png"/>
-                <NewsCard img="news3.png"/>
+                <div className="desktop-carousel">
+                    <Carousel/>
+                </div>
+                <div className="mobile-carousel">
+                    <NewsCard img="news1.png"/>
+                    <NewsCard img="news2.png"/>
+                    <NewsCard img="news3.png"/>
+                    <NewsCard img="news3.png"/>
+                </div>
             </div>
+
         </section>
     )
 }
