80e773180ef7d277c613ef126371426e1bda563b
darleet <darleet@mail.ru>
2024-04-01 16:35:49 +0300

    RS-91 Change font family on all pages

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
