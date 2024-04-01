12dffc13336c6ad08484d7eb5a801b62766f7567
darleet <darleet@mail.ru>
2024-04-01 20:23:35 +0300

    RS-80 Fixed Contacts page

--- Frontend/src/components/pages/Contacts/Contacts.tsx
+++ Frontend/src/components/pages/Contacts/Contacts.tsx
@@ -1,44 +1,58 @@
 import "./contacts.scss"
 import Circle from "../../utils/circle/Circle"
 import photo1 from "../../assets/images/contacts/photo1.png"
-import arrow from "../../assets/icons/arrow.svg"
+import diagonal_arrow from "../../assets/icons/diagonal_arrow.svg"
 import React from "react";
 
 export const Contacts = () => {
     return (
-        <section className={"contacts-section"}>
+        <section className="contacts-section">
             <Circle title="контакты" />
             <div className="container">
-                <div className={"row justify-content-center m-5"}>
-                    <div className="contacts-card text-light row align-items-center">
-                        <div className="col-md-3">
-                            <img className={"card-photo"} src={photo1} alt=""/>
+                <div className="row justify-content-center m-5 cards">
+                    <div className="row card h-100">
+                        <div className="col-4 card-photo">
+                            <img src={photo1} className="img-fluid rounded-start" alt="..."/>
                         </div>
-                        <div className="col-md-8">
-                            <h3 className={"d-inline"}>Бизиков Валерий</h3>
-                            <p>Руководитель ЦМР</p>
+                        <div className="col-8">
+                            <div className="card-body">
+                                <h5 className="card-title">Бизиков Валерий</h5>
+                                <p>Руководитель ЦМР</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="row row-cols-3">
-                    <div className="col">
-                        <p>Телефон: <a className={"text-light"} href="tel:+78005553535">8(800)555-35-35</a></p>
-                        <p>Email: <a className={"text-light"} href="mailto:cmr@bmstu.ru">cmr@bmstu.ru</a></p>
+
+                <div className="row row-cols-md-3 links m-5 justify-content-center text-center">
+                    <div className="col-md justify-content-center">
+                        <p>Телефон: <a className="text-light" href="tel:+78005553535">8(800)555-35-35</a></p>
+                        <p>Email: <a className="text-light" href="mailto:cmr@bmstu.ru">cmr@bmstu.ru</a></p>
                     </div>
-                    <div className="col">
-                        <p>Адрес: <a className={"text-light"} href="">105005, г. Москва, 2-ая Бауманская, д.5, стр.1</a>
-                        </p>
-                        <p><a className={"text-light"} href="">Схема проезда</a></p>
+                    <div className="col-md">
+                        <div>
+                            <span>Адрес: </span>
+                            <span className="text-light">105005, г. Москва, 2-ая Бауманская, д.5, стр.1</span>
+                        </div>
+                        <div>
+                            <a className="text-light" href="">Схема проезда</a>
+                            <img src={diagonal_arrow} className="diagonal-arrow" alt="..."/>
+                        </div>
                     </div>
-                    <div className="col">
-                        <p>Соцсети:</p>
-                        <p><a className={"text-light"} href="">ВКонтакте</a></p>
-                        <p><a className={"text-light"} href="">Телеграм</a></p>
+                    <div className="col-md">
+                        <p className="text-white opacity-75">Соцсети:</p>
+                        <div>
+                            <a className="text-light" href="">ВКонтакте</a>
+                            <img src={diagonal_arrow} className="diagonal-arrow" alt="..."/>
+                        </div>
+                        <div>
+                            <a className="text-light" href="">Телеграм</a>
+                            <img src={diagonal_arrow} className="diagonal-arrow" alt="..."/>
+                        </div>
                     </div>
                 </div>
             </div>
             <footer>
-                <p className={"footer-text text-black"}>Developed @ 2023 by ITS tech</p>
+                <p className="footer-text text-black">Developed @ 2023 by ITS tech</p>
             </footer>
         </section>
     )
