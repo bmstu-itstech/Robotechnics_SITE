93594e1a693ef2c1e88199d547a29a0dcd2c6ad7
darleet <darleet@mail.ru>
2024-04-01 16:36:21 +0300

    RS-81 Added text to main page

--- Frontend/src/components/pages/MainPage/MainPage.tsx
+++ Frontend/src/components/pages/MainPage/MainPage.tsx
@@ -10,9 +10,11 @@ export const MainPage = () => {
             <div className={"container-fluid"}>
                 <p className={"title-main col-11 text-uppercase text-light fw-bold lh-1"}>Центр молодежной
                     робототехники</p>
-                <p className={"text-main h3 fw-light text-light"}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p className={"text-main h3 fw-light text-light"}>Центр Молодежной Робототехники - это инновационное
+                    пространство, предназначенное для обучения и развития молодых талантов в области робототехники,
+                    искусственного интеллекта и программирования. Наша миссия - предоставить молодежи возможность
+                    исследовать и создавать будущее с помощью передовых технологий и творчества. Присоединяйтесь к
+                    нам и откройте для себя мир будущего уже сегодня!</p>
                 <button className={"button-main "}>
                     <Link className="link" to="/news_page">
                         <p className={"text-light text-uppercase fw-bold fs-4 m-0"}>подробнее</p>
