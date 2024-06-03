c6621166116e1ff0bdf88cfc80c0e42b21702e24
Robocotik <alexander.starkin@gmail.com>
2024-06-03 14:55:39 +0300

    [FIXED] logo display logic

--- Frontend/src/components/utils/logo/Logo.tsx
+++ Frontend/src/components/utils/logo/Logo.tsx
@@ -11,10 +11,10 @@ const Logo = ({ title = "" }) => {
             <div className="container logo-block mx-0 px-0 col-1 hidden_logo">
                 <img src={bauman_logo} className="logo-img logo-img-bauman img-fluid" draggable="false" alt="bauman_logo" />
             </div>
-            <div className="container logo-block mx-0 px-0 col-1 hidden_logo">
+            <div className="container logo-block mx-0 px-0 col-1 hidden_logo ">
                 <img src={crown_logo} className="logo-img img-fluid" draggable="false" alt="crown_logo" />
             </div>
-            <div className={"container logo-block mx-0 px-0 col-1"}>
+            <div className={"container logo-block mx-0 px-0 col-1 hidden_logo display-mobile"}>
                 <img src={logo} className="logo-img img-fluid" draggable="false" alt="crm_logo" />
             </div>
             <p className="title-text text-uppercase position-absolute top-50 start-50 translate-middle">{title}</p>
