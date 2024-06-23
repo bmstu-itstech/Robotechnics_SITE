2b4b35831ef3b2564e3f25d60fd35bac5c32605e
nikvoen <nikvoen@yandex.ru>
2024-06-23 23:43:12 +0300

    hardathon feat: carousel

--- Frontend/src/components/pages/Hardathon/Hardathon.tsx
+++ Frontend/src/components/pages/Hardathon/Hardathon.tsx
@@ -4,6 +4,7 @@ import arrow from "../../assets/icons/arrow.svg"
 import pic1 from "../../assets/images/events/event1.png"
 import {useEffect, useState} from "react";
 import axios from "axios";
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 interface Hardathon {
     title: string,
@@ -21,40 +22,43 @@ export const Hardathon = () => {
             }).catch(err => {
                 console.log(err);
         })
+
+        const newHardathon0 = { title: "ИНЖЕНЕРНЫЙ ВЫЗОВ 2020", photo: pic1 };
+        const newHardathon1 = { title: "ИНЖЕНЕРНЫЙ ВЫЗОВ 2021", photo: pic1 };
+        const newHardathon2 = { title: "ИНЖЕНЕРНЫЙ ВЫЗОВ 2022", photo: pic1 };
+        setHardathons([...hardathons, newHardathon0, newHardathon1, newHardathon2]);
     }, []);
 
     const slideRight = () => {
-        const board = document.querySelector('.board');
-        if (board) {
-            const firstSlide = board.firstElementChild;
-            const lastSlide = board.lastElementChild;
-            if (firstSlide) {
-                board.removeChild(firstSlide);
-                board.appendChild(firstSlide);
+        setHardathons((prevHardathons) => {
+            const updatedHardathons = [...prevHardathons];
+            const firstCard = updatedHardathons.shift();
+            if (firstCard) {
+                updatedHardathons.push(firstCard);
             }
-        }
+            return updatedHardathons;
+        });
     }
 
     const slideLeft = () => {
-        const board = document.querySelector('.board');
-        if (board) {
-            const firstSlide = board.firstElementChild;
-            const lastSlide = board.lastElementChild;
-            if (lastSlide) {
-                board.removeChild(lastSlide);
-                board.insertBefore(lastSlide, firstSlide);
+        setHardathons((prevHardathons) => {
+            const updatedHardathons = [...prevHardathons];
+            const lastCard = updatedHardathons.pop();
+            if (lastCard) {
+                updatedHardathons.unshift(lastCard);
             }
-        }
+            return updatedHardathons;
+        });
     }
 
     return (
         <section className={"events-page"}>
-            <Logo title="мероприятия"/>
+            <Logo title="Хардатоны"/>
             <div className={"board-outer d-flex justify-content-center"}>
                 <img className={"arrow-left"} src={arrow} alt="" onClick={slideLeft}/>
                 <div className="board">
-                    {hardathons.map((hardathon, index) => (
-                        <div className={"event-hard-card"}>
+                    {hardathons.slice(0, 2).map((hardathon, index) => (
+                        <div key={index} className={"event-hard-card"}>
                             <img src={hardathon.photo} alt=""/>
                             <div className={"text"}>
                                 <p>{hardathon.title}</p>
